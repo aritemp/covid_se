f6e7abb9809e5c2d51f4edfdee3c47850891ee0e
aritemp <90572979+aritemp@users.noreply.github.com>
2021-11-19 22:55:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ export ALGORITHMS=['RS256']
 export API_AUDIENCE='se_covid2021'
 export CLIENT_ID='9P90EvszrUfHRmp2AKTOiQsYUIbykQIn'
 export DATABASE_URL='postgres://qrjpxglmlwfvmj:0704636590cebfd1a6fe3b8942bf8d8c936a8bd5e12bd3765f3a6c88af9fb604@ec2-23-23-133-10.compute-1.amazonaws.com:5432/dcup748f1vqo46'
-export ADMIN='<token>'
-export USER='<token>'
+export ADMIN='Covid1234'
+export USER='Covid5678'
 #export FLASK_APP=app.py
 #export FLASK_DEBUG=true
 #flask run --reload
