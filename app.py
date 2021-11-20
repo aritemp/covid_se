74f1ec934a5d49f80255e7b1316031cd2b55f0c4
aritemp <90572979+aritemp@users.noreply.github.com>
2021-11-20 05:57:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -185,8 +185,7 @@ def create_app(test_config=None):
         print(db.session.query(Vaccination.age_group, Vaccination.region, func.sum(Vaccination.num_fully_vaccinated)).group_by(Vaccination.age_group, Vaccination.region))
         # get top 10 fully vaccinated age group & region
         vaccin = db.session.query(Vaccination.age_group, Vaccination.region, func.sum(Vaccination.num_fully_vaccinated).label('total')).group_by(Vaccination.age_group, Vaccination.region).order_by(func.sum(Vaccination.num_fully_vaccinated).desc()).limit(10).all()
-        for row in vaccin:
-            print(row._asdict())
+        print(jsonify({'result': [row._asdict() for row in vaccin]}))
         # [vac.format().get('region') for vac in vaccin]
         # abort for bad request if no vaccin info
         if vaccin is None:
